62e6c60c1ce30b4e05945dae011a84e9103a09e6
Unixhuman <220030310@stikom-bali.ac.id>
2024-01-23 09:46:33 +0800

    update terbaru

--- registrasi.php
+++ registrasi.php
@@ -26,6 +26,7 @@ if(isset($_POST['register'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Page</title>
+    <link rel="icon type=image/x-icon" href="logo2.png">
     <link rel="stylesheet" href="css/registrasiPelanggan.css">
 
 </head>
